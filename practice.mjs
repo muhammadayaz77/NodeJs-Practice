2aa1d8147160b932ffcf71cf2da9b9c938508909
Muhammad Ayaz <muhammadayaz22757@gmail.com>
2024-09-07 11:44:44 +0500

    add new features

--- practice.mjs
+++ practice.mjs
@@ -71,24 +71,87 @@
 // readFileAsync();
 
 
-import { writeFile } from "node:fs/promises";
-import { appendFile } from "node:fs";
-import { unlink } from "node:fs";
-async function writeFileAsync() {
+// import { writeFile } from "node:fs/promises";
+// import { appendFile } from "node:fs";
+// import { unlink } from "node:fs";
+// async function writeFileAsync() {
+//   try {
+//     writeFile('output1.txt','Hello World');
+//   } catch (err) {
+//     console.log(err)
+//   }
+// }
+// writeFileAsync();
+
+// appendFile('output.txt','\n Update successfully',(err) => {
+// if(err) throw err;
+// console.log('the data is appended')
+// })
+
+// unlink('output1.txt',(err) => {
+//   if(err) throw err;
+//   console.log('data deleted')
+// })
+
+// import {createServer} from 'node:http'
+// let PORT = 3000;
+// let server = createServer((req,res) => {
+//   let url = req.url;
+//   if(url == '/')
+//   {
+//     res.end(('hello World'));
+//   }else if(url == '/about'){
+//     res.end('About Page');
+//   }
+//   else{
+//     res.writeHead(404,{"Content-Type" : "text/plain"});
+//     res.end('404 Page Not Found');
+//   }
+// })
+// server.listen(PORT,() => {
+//   console.log(`http://localhost:${PORT}/`)
+// })
+
+// import { writeFileSync } from 'node:fs'
+// import { readFileSync } from 'node:fs';
+// writeFileSync('output1.txt','Hello');
+
+// let data = readFileSync('output1.txt','utf8');
+// console.log(data)
+
+import {writeFile} from 'node:fs/promises';
+import { readFile } from 'node:fs/promises';
+import { appendFile } from 'node:fs/promises';
+import { unlink } from 'node:fs/promises';
+async function  writeFileAsync() {
   try {
-    writeFile('output1.txt','Hello World');
-  } catch (err) {
-    console.log(err)
-  }
+    await writeFile('output3.txt','Hello World3')
+  } catch (error) {
+    console.log(error)
+  } 
 }
 writeFileAsync();
 
-appendFile('output.txt','\n Update successfully',(err) => {
-if(err) throw err;
-console.log('the data is appended')
-})
+async function readFileAsync() {
+  try {
+    let data = await readFile('output3.txt','utf8');
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+  
+}
+readFileAsync();
 
-unlink('output1.txt',(err) => {
-  if(err) throw err;
-  console.log('data deleted')
-})
+async function appendedFileAsync() {
+  try {
+    await appendFile('output3.txt','\n update here');
+  } catch (error) {
+    console.log(error)
+  }
+  
+}
+
+appendedFileAsync();
+
+unlink('output1.txt')
